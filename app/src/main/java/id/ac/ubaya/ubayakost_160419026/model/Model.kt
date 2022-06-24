db1fe3ce58a4c55abe4f35934b47784efe5a5b5e
seanalessandro <seanalessandrolianto@gmail.com>
2022-06-24 23:18:43 +0700

    Update model untuk implementasi Room

--- app/src/main/java/id/ac/ubaya/ubayakost_160419026/model/Model.kt
+++ app/src/main/java/id/ac/ubaya/ubayakost_160419026/model/Model.kt
@@ -1,20 +1,43 @@
 package id.ac.ubaya.ubayakost_160419026.model
 
-data class Kost(
-    val id:String?,
-    val nama:String?,
-    val ukuran:String?,
-    val kamarMandi:String?,
-    val lokasi:String?,
-    val keterangan:String?,
-    val harga:String?,
-    val urlFoto:String?
-)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity
+data class Kost(
+    @ColumnInfo(name="nama")
+    val nama:String?,
+    @ColumnInfo(name="ukuran")
+    val ukuran:String?,
+    @ColumnInfo(name="kamarMandi")
+    val kamarMandi:String?,
+    @ColumnInfo(name="lokasi")
+    val lokasi:String?,
+    @ColumnInfo(name="keterangan")
+    val keterangan:String?,
+    @ColumnInfo(name="harga")
+    val harga:String?,
+    @ColumnInfo(name="urlFoto")
+    val urlFoto:String?
+
+
+){
+    @PrimaryKey(autoGenerate = true)
+    val idKost:Int=0
+}
+
+@Entity
 data class Rating(
-    val id:String?,
+    @ColumnInfo(name="namaKos")
     val namaKos:String?,
+    @ColumnInfo(name="pemberiRating")
     val pemberiRating:String?,
+    @ColumnInfo(name="rating")
     val rating:String?,
+    @ColumnInfo(name="komentar")
     val komentar:String?,
-)
+){
+    @PrimaryKey(autoGenerate = true)
+    val idRating:Int=0
+}
